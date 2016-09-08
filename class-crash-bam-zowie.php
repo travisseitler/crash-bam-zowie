27a7f3c7e2e0d1dd7bc53bfbfa025ddd5ccbb182
Travis Seitler <travis@webseitler.com>
2016-09-07 23:32:46 -0400

    v0.1.1 basic plugin functions and scaffolding

---
+++ class-crash-bam-zowie.php
@@ -0,0 +1,197 @@
+<?php
+
+class Crash_Bam_Zowie {
+
+	/**
+	 * Unique Identifier
+	 *
+	 * @since 0.1.1
+	 */
+	protected $plugin_version = '0.1.1';
+
+	/**
+	 * Unique Identifier
+	 *
+	 * @since 0.1.1
+	 */
+	protected $plugin_slug = 'crash-bam-zowie';
+
+	/**
+	 * Initialization: Let's get it started in here
+	 *
+	 * @since 0.1.1
+	 */
+	public function init() {
+
+		// Define custom post type on init
+		add_action( 'init',                     array( $this, 'define_posttype' ) );
+		add_action( 'admin_init',               array( $this, 'define_posttype' ) );
+
+		// Define custom taxonomy on init
+		add_action( 'init',                     array( $this, 'define_taxonomy' ) );
+		add_action( 'admin_init',               array( $this, 'define_taxonomy' ) );
+		
+		// Add a custom Settings Page in the Admin
+		add_action( 'admin_menu',               array( $this, 'settings_page' ) );
+
+		// Add the ability to target specific term pages in the admin for CSS/JS
+		add_filter( 'admin_body_class',         array( $this, 'admin_body_class' ) );
+
+	}
+
+	/**
+	 * Define Custom Post Type: Comic Page
+	 *
+	 * @since 0.1.1
+	 */
+	function define_posttype() {
+
+		/**
+		 * Post Thumbnails: WE HAZ DEM.
+		 */
+		add_theme_support('post-thumbnails');
+
+		/**
+		 * Define the Custom Post Type
+		 */
+		$top_level_name = 'Comic Pages';
+		$front_name     = 'Comic Pages';
+		$singular_name  = 'Comic Page';
+		$plural_name    = 'Comic Pages';
+		$args = array(
+			'public'                => true,
+			'menu_position'         => 31,
+			'menu_icon'             => "dashicons-layout",
+			'capability_type'       => array( 'post', 'posts' ),
+			'map_meta_cap'          => true,
+			'hierarchical'          => false,
+			'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author' ),
+			'has_archive'           => true,
+			'can_export'            => true,
+			'labels'                => array(
+				'name'                       => $front_name,
+				'singular_name'              => $singular_name,
+				'menu_name'                  => $top_level_name,
+				'all_items'                  => 'All ' . $plural_name,
+				'add_new'                    => 'Add ' . $singular_name,
+				'add_new_item'               => 'Add ' . $singular_name,
+				'edit_item'                  => 'Edit ' . $singular_name,
+				'new_item'                   => 'New ' . $singular_name,
+				'view_item'                  => 'View ' . $singular_name,
+				'search_items'               => 'Search ' . $plural_name,
+				'not_found'                  => 'No ' . $plural_name . ' found',
+				'not_found_in_trash'         => 'No ' . $plural_name . ' found in Trash',
+			),
+			'queryable'             => true,
+		);
+
+		register_post_type( $this->plugin_slug . '-pages', $args );
+
+	}
+
+	/**
+	 * Define Custom Taxonomy: Comic Issues
+	 *
+	 * @since 0.1.1
+	 */
+	function define_taxonomy() {
+
+		$singular_name = 'Comic Issue';
+		$plural_name = 'Comic Issues';
+
+		$args = array(
+			'public'            => true,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'hierarchical'      => true,
+			'labels'            => array(
+				'name'                       => $plural_name,
+				'singular_name'              => $singular_name,
+				'menu_name'                  => $plural_name,
+				'all_items'                  => 'All ' . $plural_name,
+				'edit_item'                  => 'Edit ' . $plural_name,
+				'view_item'                  => 'View ' . $singular_name,
+				'update_item'                => 'Update ' . $plural_name,
+				'add_new_item'               => 'Add ' . $singular_name,
+				'new_item_name'              => 'New ' . $singular_name,
+				'search_items'               => 'Search ' . $plural_name,
+				'popular_items'              => 'Popular ' . $plural_name,
+				'separate_items_with_commas' => 'Separate ' . $plural_name . ' with commas',
+				'add_or_remove_items'        => 'Add or remove ' . $plural_name,
+				'choose_from_most_used'      => 'Choose from the most used ' . $plural_name,
+				'not_found'                  => 'No ' . $plural_name . ' found',
+			),
+		);
+
+		register_taxonomy( $this->plugin_slug . '-issues', $this->plugin_slug . '-pages', $args );
+
+	}
+
+    /**
+     * Add a link to the Settings page in the WordPress Admin Menu
+	 * 
+	 * @since 0.1.1
+     */
+    function settings_page() {
+
+        add_submenu_page(
+			$this->plugin_slug . '-pages',
+            'CRASH! BAM! ZOWIE! Webcomic Management Settings',
+			'CBZ Settings',
+			'manage_options',
+			'cbz_settings',
+            array( $this, 'create_settings_page' )
+        );
+
+    }
+
+    /**
+     * Create a Settings page
+	 * 
+	 * @since 0.1.1
+     */
+    function create_settings_page() {
+		
+		$title = 'CRASH! BAM! ZOWIE! Webcomic Management Settings';
+		
+		print '<div class="wrap">';
+	
+		print '<h1>' . $title . '</h1>';
+		
+		print '</div>'; // .wrap
+		
+    }
+
+	/**
+	 * Admin: Add term-specific classes to body tag
+	 * 
+	 * With extra classes, we can more cleanly target specific Admin
+	 * pages when applying custom CSS rules
+	 *
+	 * @since 0.1.1
+	 */
+	function admin_body_class( $classes ) {
+
+		$screen = get_current_screen();
+
+		if ( 'term' != $screen->base || $this->plugin_slug != $screen->taxonomy ) { return $classes; }
+		
+		$term_ID = absint( $_REQUEST['tag_ID'] );
+		$term    = get_term( $term_ID, $this->plugin_slug, OBJECT, 'edit' );
+
+		$classes = explode( ' ', $classes );
+
+		$additional_classes = array(
+			'term-id-' . $term->term_id,
+			'term-' . $this->plugin_slug . '-' . $term->slug,
+		);
+
+		$classes = array_merge( $classes, $additional_classes );
+
+		$classes = implode( ' ', $classes );
+
+		return $classes;
+
+	}
+
+}
--- crash-bam-zowie.php
+++ crash-bam-zowie.php
@@ -1,23 +1,53 @@
 <?php
-/*
-Plugin Name: CRASH! BAM! ZOWIE!
-Plugin URI:  http://www.webseitler.com/wordpress-plugins/crash-bam-zowie/
-Description: Easily manage and publish your webcomics with WordPress.
-Version:     0.1
-Author:      Travis Seitler
-Author URI:  http://www.webseitler.com/
-License:     GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Domain Path: /languages/
-Text Domain: crash-bam-zowie
-*/
-
 /**
- * @package    CRASH! BAM! ZOWIE!
- * @version    0.1
+ * @package    WordPress
+ * @subpackage Crash_Bam_Zowie
+ * @link       http://www.webseitler.com/wordpress-plugins/crash-bam-zowie/
+ * @version    0.1.1
  * @author     Travis Seitler <travis@webseitler.com>
  * @copyright  Copyright (c) 2016, Travis Seitler
- * @link       http://www.webseitler.com/wordpress-plugins/crash-bam-zowie/
  * @license    https://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * @wordpress-plugin
+ * Plugin Name: CRASH! BAM! ZOWIE!
+ * Plugin URI:  http://www.webseitler.com/wordpress-plugins/crash-bam-zowie/
+ * Description: Easily manage and publish your webcomics with WordPress.
+ * Text Domain: crash-bam-zowie
+ * Version:     0.1.1
+ * Domain Path: /languages/
+ * Author:      Travis Seitler
+ * Author URI:  http://www.webseitler.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Includes the class that provides the functionality for the plugin
+include_once( 'class-crash-bam-zowie.php' );
+
+// Include code for admin options page
+include_once( 'crash-bam-zowie-admin.php' );
+
+/**
+ * Instantiates the plugin and initializes the functionality necessary for WordPress
+ *
+ * @since 0.1.1
+ */
+$crash_bam_zowie_plugin = new Crash_Bam_Zowie();
+$crash_bam_zowie_plugin->init();
+
+/**
+ * Since we define some custom rewrite rules in this plugin, we'll automatically
+ * flush the rewrite rules cache any time it's activated or deactivated.
+ *
+ * @since 0.1.1
+ */
+function crash_bam_zowie_flush_rewrites() {
+	flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'crash_bam_zowie_flush_rewrites' );
+register_deactivation_hook( __FILE__, 'crash_bam_zowie_flush_rewrites' );
