7da0d93e1e0dfcfc113b727f06280aa6820d2018
Travis Seitler <travis@webseitler.com>
2016-09-07 21:47:02 -0400

    v0.1 - Plugin file init. Readme and license info updated

---
+++ crash-bam-zowie.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: CRASH! BAM! ZOWIE!
+Plugin URI:  http://www.webseitler.com/wordpress-plugins/crash-bam-zowie/
+Description: Easily manage and publish your webcomics with WordPress.
+Version:     0.1
+Author:      Travis Seitler
+Author URI:  http://www.webseitler.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages/
+Text Domain: crash-bam-zowie
+*/
+
+/**
+ * @package    CRASH! BAM! ZOWIE!
+ * @version    0.1
+ * @author     Travis Seitler <travis@webseitler.com>
+ * @copyright  Copyright (c) 2016, Travis Seitler
+ * @link       http://www.webseitler.com/wordpress-plugins/crash-bam-zowie/
+ * @license    https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
