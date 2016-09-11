ee252554837219e36d1cf933fab2b697aedafb1a
Travis Seitler <travis@webseitler.com>
2016-09-11 14:55:57 -0400

    Big Update: Series info summaries and management admin page
    Also includes the option to associate images with series and issue tags.

--- class-crash-bam-zowie.php
+++ class-crash-bam-zowie.php
@@ -24,21 +24,30 @@ class Crash_Bam_Zowie {
 	public function init() {
 
 		// Define custom post type on init
-		add_action( 'init',                     array( $this, 'define_posttype' ) );
-		add_action( 'admin_init',               array( $this, 'define_posttype' ) );
+		add_action( 'init',                                           array( $this, 'define_posttype' ) );
+		add_action( 'admin_init',                                     array( $this, 'define_posttype' ) );
 
 		// Define custom taxonomy on init
-		add_action( 'init',                     array( $this, 'define_taxonomy' ) );
-		add_action( 'admin_init',               array( $this, 'define_taxonomy' ) );
+		add_action( 'init',                                           array( $this, 'define_taxonomy' ) );
+		add_action( 'admin_init',                                     array( $this, 'define_taxonomy' ) );
 
-		// Adjust the admin display for
-		add_action( 'do_meta_boxes',            array( $this, 'change_image_box' ) );
+		// Adjust the admin display for our Custom Post Type
+		add_action( 'do_meta_boxes',                                  array( $this, 'change_featured_image_box' ) );
+
+		// Adjust the admin display for our Custom Taxonomy
+		// Thanks to CatapultThemes at https://catapultthemes.com/adding-an-image-upload-field-to-categories/
+    add_action( $this->plugin_slug . '-issues_add_form_fields',   array( $this, 'add_issue_image' ), 10, 2 );
+    add_action( 'created_' . $this->plugin_slug . '-issues',      array( $this, 'save_issue_image' ), 10, 2 );
+    add_action( $this->plugin_slug . '-issues_edit_form_fields',  array( $this, 'update_issue_image' ), 10, 2 );
+    add_action( 'edited_'.$this->plugin_slug . '-issues',         array( $this, 'updated_issue_image' ), 10, 2 );
 
 		// Add a custom Settings Page in the Admin
-		add_action( 'admin_menu',               array( $this, 'settings_page' ) );
+		add_action( 'admin_menu',                                     array( $this, 'settings_page' ) );
 
-		// Add the ability to target specific term pages in the admin for CSS/JS
-		add_filter( 'admin_body_class',         array( $this, 'admin_body_class' ) );
+		// Add CSS classes to target specific term pages in the admin
+		add_filter( 'admin_body_class',                               array( $this, 'admin_body_class' ) );
+    add_action( 'admin_footer',                                   array( $this, 'admin_add_script' ) );
+		add_action( 'admin_enqueue_scripts',                          array( $this, 'load_wp_media_files' ) );
 
 	}
 
@@ -99,8 +108,8 @@ class Crash_Bam_Zowie {
 	 */
 	function define_taxonomy() {
 
-		$singular_name = 'Comic Issue';
-		$plural_name = 'Comic Issues';
+		$singular_name = 'Series/Issue';
+		$plural_name = 'Series &amp; Issues';
 
 		$args = array(
 			'public'            => true,
@@ -128,7 +137,101 @@ class Crash_Bam_Zowie {
 
 		register_taxonomy( $this->plugin_slug . '-issues', $this->plugin_slug, $args );
 
+		$args = array(
+		    'sanitize_callback' => 'sanitize_my_meta_key',
+		    'auth_callback' => 'authorize_my_meta_key',
+		    'type' => 'string',
+		    'description' => 'My registered meta key',
+		    'single' => true,
+		    'show_in_rest' => true,
+		);
+		register_meta( $this->plugin_slug . '-issues', 'cbz_cover_logo', $args );
+
 	}
+
+	/**
+	 * Admin: Modify the appearance of the Featured Image block
+	 *
+	 * @since 0.1.1
+	 */
+	function change_featured_image_box()	{
+
+	    remove_meta_box( 'postimagediv', $this->plugin_slug, 'side' );
+
+	    add_meta_box( 'postimagediv', __('Comic Art'), 'post_thumbnail_meta_box', $this->plugin_slug, 'normal', 'high' );
+
+	}
+
+  /**
+   * Add a Cover Art form field in the New Issue/Series page
+	 *
+   * @since 0.1.1
+   */
+  public function add_issue_image( $taxonomy ) { ?>
+    <div class="form-field term-group">
+      <label for="issues-image-id"><?php _e( 'Cover Art' ); ?></label>
+      <input type="hidden" id="issues-image-id" name="issues-image-id" class="custom_media_url" value="">
+      <div id="issues-image-wrapper"></div>
+      <p>
+        <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image' ); ?>" />
+        <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image' ); ?>" />
+     </p>
+    </div>
+  <?php
+  }
+
+  /**
+   * Save the Cover Art form field
+	 *
+   * @since 0.1.1
+	 */
+  public function save_issue_image( $term_id, $tt_id ) {
+    if( isset( $_POST['issues-image-id'] ) && '' !== $_POST['issues-image-id'] ){
+      $image = $_POST['issues-image-id'];
+      add_term_meta( $term_id, 'issues-image-id', $image, true );
+    }
+  }
+
+  /**
+   * Edit the form field
+	 *
+   * @since 0.1.1
+   */
+  public function update_issue_image( $term, $taxonomy ) { ?>
+    <tr class="form-field term-group-wrap">
+      <th scope="row">
+        <label for="issues-image-id"><?php _e( 'Cover Art' ); ?></label>
+      </th>
+      <td>
+        <?php $image_id = get_term_meta ( $term -> term_id, 'issues-image-id', true ); ?>
+        <input type="hidden" id="issues-image-id" name="issues-image-id" value="<?php echo $image_id; ?>">
+        <div id="issues-image-wrapper">
+          <?php if ( $image_id ) { ?>
+            <?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
+          <?php } ?>
+        </div>
+        <p>
+          <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image' ); ?>" />
+          <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image' ); ?>" />
+        </p>
+      </td>
+    </tr>
+  <?php
+  }
+
+	/**
+	 * Update the form field value
+	 *
+   * @since 0.1.1
+   */
+	 public function updated_issue_image( $term_id, $tt_id ) {
+	   if( isset( $_POST['issues-image-id'] ) && '' !== $_POST['issues-image-id'] ){
+	     $image = $_POST['issues-image-id'];
+	     update_term_meta( $term_id, 'issues-image-id', $image );
+	   } else {
+	     update_term_meta( $term_id, 'issues-image-id', '' );
+	   }
+	 }
 
   /**
    * Add a link to the Settings page in the WordPress Admin Menu
@@ -139,8 +242,8 @@ class Crash_Bam_Zowie {
 
     add_submenu_page(
 			'edit.php?post_type=' . $this->plugin_slug,
-      'CRASH! BAM! ZOWIE! Webcomic Management Settings',
-			'Settings',
+      'Webcomic Settings',
+			'Webcomic Settings',
 			'manage_options',
 			'cbz_settings',
       array( $this, 'create_settings_page' )
@@ -154,6 +257,12 @@ class Crash_Bam_Zowie {
 	 * @since 0.1.1
    */
   function create_settings_page() {
+
+		$terms = get_terms( array(
+			'taxonomy'   => $this->plugin_slug . '-issues',
+			'parent'     => 0,
+			'hide_empty' => false,
+		) );
 
 		// Include code for admin options page
 		include_once( 'crash-bam-zowie-admin.php' );
@@ -192,17 +301,57 @@ class Crash_Bam_Zowie {
 
 	}
 
-	/**
-	 * Admin: Modify the appearance of the Featured Image block
-	 *
-	 * @since 0.1.1
-	 */
-	function change_image_box()	{
+ /*
+	* Add script
+	* @since 1.0.0
+	*/
+	public function admin_add_script() { ?>
+		<script>
+			jQuery(document).ready( function($) {
+				function ct_media_upload(button_class) {
+					var _custom_media = true,
+					_orig_send_attachment = wp.media.editor.send.attachment;
+					$('body').on('click', button_class, function(e) {
+						var button_id = '#'+$(this).attr('id');
+						var send_attachment_bkp = wp.media.editor.send.attachment;
+						var button = $(button_id);
+						_custom_media = true;
+						wp.media.editor.send.attachment = function(props, attachment){
+							if ( _custom_media ) {
+								$('#issues-image-id').val(attachment.id);
+								$('#issues-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;float:none;" />');
+								$('#issues-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
+							} else {
+								return _orig_send_attachment.apply( button_id, [props, attachment] );
+							}
+						 }
+					wp.media.editor.open(button);
+					return false;
+				});
+			}
+			ct_media_upload('.ct_tax_media_button.button');
+			$('body').on('click','.ct_tax_media_remove',function(){
+				$('#issues-image-id').val('');
+				$('#issues-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;float:none;" />');
+			});
+			// Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
+			$(document).ajaxComplete(function(event, xhr, settings) {
+				var queryStringArr = settings.data.split('&');
+				if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+					var xml = xhr.responseXML;
+					$response = $(xml).find('term_id').text();
+					if($response!=""){
+						// Clear the thumb image
+						$('#issues-image-wrapper').html('');
+					}
+				}
+			});
+		});
+	</script>
+	<?php }
 
-	    remove_meta_box( 'postimagediv', $this->plugin_slug, 'side' );
-
-	    add_meta_box( 'postimagediv', __('Comic Art'), 'post_thumbnail_meta_box', $this->plugin_slug, 'normal', 'high' );
-
+	function load_wp_media_files() {
+		wp_enqueue_media();
 	}
 
 }
--- crash-bam-zowie-admin.php
+++ crash-bam-zowie-admin.php
@@ -5,6 +5,78 @@
 ?>
 <div class="wrap">
 
-  <h1>Webcomics Settings</h1>
+  <h1>Webcomics Settings <a class="page-title-action" href="/wp-admin/edit-tags.php?post_type=crash-bam-zowie&taxonomy=<?= $this->plugin_slug . '-issues' ?>">Add Webcomic Series</a></h1>
+
+	<div class="the_series clearfix">
+
+	<?php
+
+  foreach( $terms as $term ) {
+
+			// print_r( $term );
+
+			$meta = get_term_meta( $term->term_id );
+
+			// print_r( $meta );
+
+      $image_id = get_term_meta ( $term->term_id, 'issues-image-id', true );
+
+			print '<section id="' . $term->slug . '-block" class="coi-center-block card row cf">';
+
+			print '<div class="contain">';
+
+				// Title and Description
+
+        if ( $image_id ) {
+          print wp_get_attachment_image( $image_id, 'large', false, array( 'style' => 'display:block; max-width:100%; height:auto; margin:1em auto;' ) );
+        }
+
+				print '<h2>' . $term->name . ' <a class="button button-small" href="/wp-admin/term.php?taxonomy=' . $this->plugin_slug . '&tag_ID=' . $term->term_id . '">Edit</a></h2>';
+
+				print '<div class="description">' . $term->description . '</div>';
+
+				// Content Counts
+
+				print '<dl class="summary">';
+
+          // Chapters
+
+          print '<dt><strong>Chapters/Issues:</strong></dt>';
+
+          $children_count = wp_count_terms( $term->taxonomy, array( 'child_of' => $term->term_id ) );
+
+					print '<dd>' . intval( wp_count_terms( $term->taxonomy, array( 'child_of' => $term->term_id ) ) ) . '</dd>';
+
+					// Pages
+
+          print '<dt><strong>Pages:</strong></dt>';
+
+          $args = array(
+            'post_type' => $this->plugin_slug,
+          	'tax_query' => array(
+          		array(
+          			'taxonomy' => $term->taxonomy,
+          			'field'    => 'term_id',
+          			'terms'    => $term->term_id,
+          		),
+          	),
+          );
+          $the_query = new WP_Query( $args );
+
+					print '<dd>' . intval( $the_query->found_posts ) . '</dd>';
+
+				print '</dl>';
+
+			print '</div>';
+
+			print '</section>';
+
+		}
+
+    ?>
+
+  </div><?php // .the_centers ?>
+
+	<div class="block"><a class="button button-primary" href="/wp-admin/edit-tags.php?post_type=crash-bam-zowie&taxonomy=<?= $this->plugin_slug . '-issues' ?>">Add Webcomic Series</a></div>
 
 </div><?php // .wrap ?>
